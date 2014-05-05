95dee8a03ef372ea6d3fccdbcd8db6acdd49b23b
MeePoohz <guartcoolz@gmail.com>
2014-05-05 14:47:37 +0700

    Change Search SQL LIKE

--- app/src/main/java/com/busbmta/app/MainActivity.java
+++ app/src/main/java/com/busbmta/app/MainActivity.java
@@ -1,29 +1,34 @@
 package com.busbmta.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import java.util.ArrayList;
-import android.app.Activity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.Window;
+import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
     SQLiteDatabase mDb;
     MyDbHelper mHelper;
     Cursor mCursor;
+    ListView listView;
+    ArrayList<String> BusNoArr = new ArrayList<String>();
+    ArrayList<String> BusTimeArr = new ArrayList<String>();
+    ArrayList<String> BusWayArr = new ArrayList<String>();
+    EditText editText1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = (ListView)findViewById(R.id.listView1);
+        listView = (ListView)findViewById(R.id.listView1);
 
         mHelper = new MyDbHelper(this);
         mDb = mHelper.getWritableDatabase();
@@ -32,9 +37,6 @@ public class MainActivity extends ActionBarActivity {
                 + ", " + MyDbHelper.COL_BUSWAY
                 + " FROM " + MyDbHelper.TABLE_NAME + " ORDER BY " + MyDbHelper.COL_BUSNO + " ASC",null);
 
-        ArrayList<String> BusNoArr = new ArrayList<String>();
-        ArrayList<String> BusTimeArr = new ArrayList<String>();
-        ArrayList<String> BusWayArr = new ArrayList<String>();
         mCursor.moveToFirst();
 
         while ( !mCursor.isAfterLast() ){
@@ -46,6 +48,48 @@ public class MainActivity extends ActionBarActivity {
 
         ArrayAdapter<String> adapterDir = new CustomListViewAdapter(this, android.R.layout.simple_list_item_1, BusNoArr, BusTimeArr, BusWayArr);
         listView.setAdapter(adapterDir);
+
+        editText1 = (EditText)findViewById(R.id.editText1);
+        editText1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                ArrayList<String> BusNoSrc = new ArrayList<String>();
+                ArrayList<String> BusTimeSrc = new ArrayList<String>();
+                ArrayList<String> BusWaySrc = new ArrayList<String>();
+                if (editText1.getText().toString().equals("")) {
+                    ArrayAdapter<String> adapterDir = new CustomListViewAdapter(MainActivity.this, android.R.layout.simple_list_item_1, BusNoArr, BusTimeArr, BusWayArr);
+                    listView.setAdapter(adapterDir);
+                }
+                else {
+
+                    mCursor = mDb.rawQuery("SELECT " + MyDbHelper.COL_BUSNO
+                            + ", " + MyDbHelper.COL_BUSTIME
+                            + ", " + MyDbHelper.COL_BUSWAY
+                            + " FROM " + MyDbHelper.TABLE_NAME + " WHERE " + MyDbHelper.COL_BUSWAY + " LIKE '%" + editText1.getText().toString() + "%' ORDER BY " + MyDbHelper.COL_BUSNO + " ASC", null);
+
+                    mCursor.moveToFirst();
+
+                    while (!mCursor.isAfterLast()) {
+                        BusNoSrc.add(mCursor.getString(mCursor.getColumnIndex(MyDbHelper.COL_BUSNO)));
+                        BusTimeSrc.add(mCursor.getString(mCursor.getColumnIndex(MyDbHelper.COL_BUSTIME)));
+                        BusWaySrc.add(mCursor.getString(mCursor.getColumnIndex(MyDbHelper.COL_BUSWAY)));
+                        mCursor.moveToNext();
+                    }
+                    ArrayAdapter<String> adapterDir = new CustomListViewAdapter(MainActivity.this, android.R.layout.simple_list_item_1, BusNoSrc, BusTimeSrc, BusWaySrc);
+                    listView.setAdapter(adapterDir);
+                }
+            }
+        });
     }
 
     public void onPause() {
